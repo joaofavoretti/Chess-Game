636972fb6bc9d273e2a1e84ac883f6e6e8a53e3f
joaofavoretti <contas.joaopedro.favoretti@gmail.com>
2025-05-20 08:42:17 -0300

    Adding pawn attack and knight move to move_gen

--- src/move_gen.zig
+++ src/move_gen.zig
@@ -32,6 +32,16 @@ fn isWhiteOrBlackPromotionSquare(targetSquare: u6) bool {
         (targetSquare ^ 0b111000) & 0b111000 == 0b111000;
 }
 
+fn knightAttacks(knightBitboard: Bitboard) Bitboard {
+    const l1 = (knightBitboard >> 1) & 0x7f7f7f7f7f7f7f7f;
+    const l2 = (knightBitboard >> 2) & 0x3f3f3f3f3f3f3f3f;
+    const r1 = (knightBitboard << 1) & 0xfefefefefefefefe;
+    const r2 = (knightBitboard << 2) & 0xfcfcfcfcfcfcfcfc;
+    const h1 = l1 | r1;
+    const h2 = l2 | r2;
+    return (h1 << 16) | (h1 >> 16) | (h2 << 8) | (h2 >> 8);
+}
+
 pub const MoveGen = struct {
     pseudoLegalMoves: std.ArrayList(Move),
 
@@ -52,6 +62,8 @@ pub const MoveGen = struct {
     pub fn update(self: *MoveGen, board: *Board) void {
         self.clear();
         self.genPawnPushes(board);
+        self.genPawnAttacks(board);
+        self.genKnightMoves(board);
     }
 
     fn genPawnPushes(self: *MoveGen, board: *Board) void {
@@ -117,6 +129,179 @@ pub const MoveGen = struct {
             self.pseudoLegalMoves.append(move) catch unreachable;
             doublePushTarget &= ~(@as(u64, 1) << targetSquare);
             pawnAble2DblPush &= ~(@as(u64, 1) << originSquare);
+        }
+    }
+
+    fn genPawnAttacks(self: *MoveGen, board: *Board) void {
+        const colorToMove = board.pieceToMove;
+
+        const pawnBitboard = board.boards[@intFromEnum(colorToMove)][@intFromEnum(PieceType.Pawn)];
+
+        const pawnEastAttacks: Bitboard = switch (colorToMove) {
+            PieceColor.White => pawnAttackUtils.whitePawnEastAttack(pawnBitboard),
+            PieceColor.Black => pawnAttackUtils.blackPawnEastAttack(pawnBitboard),
+        };
+
+        const pawnWestAttacks: Bitboard = switch (colorToMove) {
+            PieceColor.White => pawnAttackUtils.whitePawnWestAttack(pawnBitboard),
+            PieceColor.Black => pawnAttackUtils.blackPawnWestAttack(pawnBitboard),
+        };
+
+        var oppositeColorBitboard = board.getColorBitboard(colorToMove.opposite());
+
+        // Check the EnPassant target square as valid
+        if (board.enPassantTarget) |enPassantTarget| {
+            oppositeColorBitboard |= (@as(u64, 1) << enPassantTarget);
+        }
+
+        var pawnEastAttackTarget = pawnEastAttacks & oppositeColorBitboard;
+        var pawnEastAble2Capture = switch (colorToMove) {
+            PieceColor.White => pawnAttackUtils.whitePawnAble2CaptureEast(pawnEastAttackTarget),
+            PieceColor.Black => pawnAttackUtils.blackPawnAble2CaptureEast(pawnEastAttackTarget),
+        };
+
+        while (pawnEastAttackTarget != 0 and pawnEastAble2Capture != 0) {
+            const originSquare: u6 = @intCast(@ctz(pawnEastAble2Capture));
+            const targetSquare: u6 = @intCast(@ctz(pawnEastAttackTarget));
+
+            const capturedPiece = board.getPiece(targetSquare);
+
+            var move = Move.init(
+                originSquare,
+                targetSquare,
+                board,
+                .{ .Capture = .{ .capturedPiece = capturedPiece } },
+            );
+
+            if (!capturedPiece.valid) {
+                move = Move.init(
+                    originSquare,
+                    targetSquare,
+                    board,
+                    .{ .EnPassant = .{
+                        .capturedPiece = Piece.init(
+                            colorToMove.opposite(),
+                            PieceType.Pawn,
+                        ),
+                    } },
+                );
+            }
+
+            if (isWhiteOrBlackPromotionSquare(targetSquare)) {
+                move = Move.init(
+                    originSquare,
+                    targetSquare,
+                    board,
+                    .{ .QueenPromotionCapture = .{
+                        .capturedPiece = capturedPiece,
+                    } },
+                );
+            }
+
+            self.pseudoLegalMoves.append(move) catch unreachable;
+            pawnEastAttackTarget &= ~(@as(u64, 1) << targetSquare);
+            pawnEastAble2Capture &= ~(@as(u64, 1) << originSquare);
+        }
+
+        var pawnWestAttackTarget = pawnWestAttacks & oppositeColorBitboard;
+        var pawnWestAble2Capture = switch (colorToMove) {
+            PieceColor.White => pawnAttackUtils.whitePawnAble2CaptureWest(pawnWestAttackTarget),
+            PieceColor.Black => pawnAttackUtils.blackPawnAble2CaptureWest(pawnWestAttackTarget),
+        };
+
+        while (pawnWestAttackTarget != 0 and pawnWestAble2Capture != 0) {
+            const originSquare: u6 = @intCast(@ctz(pawnWestAble2Capture));
+            const targetSquare: u6 = @intCast(@ctz(pawnWestAttackTarget));
+
+            const capturedPiece = board.getPiece(targetSquare);
+
+            var move = Move.init(
+                originSquare,
+                targetSquare,
+                board,
+                .{ .Capture = .{ .capturedPiece = capturedPiece } },
+            );
+
+            // Condition to be an enPassant capture
+            if (!capturedPiece.valid) {
+                move = Move.init(
+                    originSquare,
+                    targetSquare,
+                    board,
+                    .{ .EnPassant = .{
+                        .capturedPiece = Piece.init(
+                            colorToMove.opposite(),
+                            PieceType.Pawn,
+                        ),
+                    } },
+                );
+            }
+
+            // Capture with promotion
+            if (isWhiteOrBlackPromotionSquare(targetSquare)) {
+                move = Move.init(
+                    originSquare,
+                    targetSquare,
+                    board,
+                    .{ .QueenPromotionCapture = .{
+                        .capturedPiece = capturedPiece,
+                    } },
+                );
+            }
+
+            self.pseudoLegalMoves.append(move) catch unreachable;
+            pawnWestAttackTarget &= ~(@as(u64, 1) << targetSquare);
+            pawnWestAble2Capture &= ~(@as(u64, 1) << originSquare);
+        }
+    }
+
+    fn genKnightMoves(self: *MoveGen, board: *Board) void {
+        const colorToMove = board.pieceToMove;
+        var knightBitboard = board.boards[@intFromEnum(colorToMove)][@intFromEnum(PieceType.Knight)];
+        const availableSquares = ~board.getColorBitboard(colorToMove);
+
+        while (knightBitboard != 0) {
+            const originSquare: u6 = @intCast(@ctz(knightBitboard));
+            var attackTarget = knightAttacks(@as(u64, 1) << originSquare);
+            attackTarget &= availableSquares;
+
+            var captureTarget = attackTarget & board.getColorBitboard(colorToMove.opposite());
+            attackTarget &= ~captureTarget;
+
+            // Capture moves
+            while (captureTarget != 0) {
+                const targetSquare: u6 = @intCast(@ctz(captureTarget));
+
+                const capturedPiece = board.getPieceFromColor(targetSquare, colorToMove.opposite());
+
+                const move = Move.init(
+                    originSquare,
+                    targetSquare,
+                    board,
+                    .{ .Capture = .{ .capturedPiece = capturedPiece } },
+                );
+
+                self.pseudoLegalMoves.append(move) catch unreachable;
+
+                captureTarget &= ~(@as(u64, 1) << targetSquare);
+            }
+
+            // Quiet moves
+            while (attackTarget != 0) {
+                const targetSquare: u6 = @intCast(@ctz(attackTarget));
+
+                const move = Move.init(
+                    originSquare,
+                    targetSquare,
+                    board,
+                    .{ .QuietMove = .{} },
+                );
+
+                self.pseudoLegalMoves.append(move) catch unreachable;
+
+                attackTarget &= ~(@as(u64, 1) << targetSquare);
+            }
+            knightBitboard &= ~(@as(u64, 1) << originSquare);
         }
     }
 };
